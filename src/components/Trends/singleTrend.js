7a5d291df4475c213b33641801fd7c6c8b4f74dd
Andres Medrano <amedra13@gmail.com>
2021-03-06 18:44:00 -0800

    begin structure of Trend Secton

--- src/components/Trends/singleTrend.js
+++ src/components/Trends/singleTrend.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const singleTrend = ({trend}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const SingleTrend = ({ trend }) => {
+	return (
+		<div className="singleTrend">
+			<h6>Trending {trend.section}</h6>
+			<h3>{trend.hashtag}</h3>
+			<h2>{trend.numberOfTweets}</h2>
+			<h6>{trend.numberOfPeople} people are Tweeting about this</h6>
+		</div>
+	);
+};
 
-export default singleTrend
+export default SingleTrend;
--- src/components/mainPage/Trend.js
+++ src/components/mainPage/Trend.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import SingleTrend from '../Trends/SingleTrend';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	searchTwitter: {
+		border: 'none',
+		backgroundColor: 'rgba(187, 214, 240, 0.301)',
+		padding: '6px',
+		borderRadius: '25px',
+		width: '90%',
+	},
+}));
 
 const Trend = () => {
+	const classes = useStyles();
+
+	const fakeTrend = {
+		section: 'worldwide',
+		hashTag: '#BreakingNews',
+		numberOfTweets: '125k',
+		numberOfPeople: 5123,
+	};
 	return (
 		<div className="trend">
-			<h1>Trend Component</h1>
+			<div className="trendContainer">
+				<div className="trend__searchbar">
+					<Input
+						className={classes.searchTwitter}
+						placeholder="Seach Twitter"
+						disableUnderline
+						fullWidth
+						startAdornment={
+							<InputAdornment position="start">
+								<SearchIcon />
+							</InputAdornment>
+						}
+					/>
+				</div>
+
+				<div className="trend__sections">
+					<div className="trend__title">
+						<h2>Trends for you</h2>
+						<SettingsOutlinedIcon style={{ color: '#00b4d8' }} />
+					</div>
+
+					<SingleTrend trend={fakeTrend} />
+					<SingleTrend trend={fakeTrend} />
+					<SingleTrend trend={fakeTrend} />
+					<div className="trend__showMore">
+						<h5>Show More</h5>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
