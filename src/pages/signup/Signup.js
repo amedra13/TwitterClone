3e3a42963a21d77b00ec8291c857797f874d2d81
Andres Medrano <amedra13@gmail.com>
2021-03-07 21:39:53 -0800

    add login input fields and style

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import SearchIcon from '@material-ui/icons/Search';
 import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
 import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	inputField: {
+		margin: '5px 0',
+		color: '#00b4d8 !important',
+	},
+	button: {
+		backgroundColor: '#00b4d8',
+		color: 'white',
+	},
+}));
 
 const Signup = () => {
+	const classes = useStyles();
+
 	return (
 		<div className="signup">
 			<div className="signup__left">
@@ -23,7 +40,33 @@ const Signup = () => {
 				</div>
 			</div>
 			<div className="signup__right">
-				<h1>SignUp Component</h1>
+				<div className="signup__rightContainer">
+					<div className="signup__rightHeader">
+						<TwitterIcon style={{ color: '#00b4d8' }} fontSize="large" />
+						<h2>See what is happening in the world right now</h2>
+						<h5>Join Twitter today.</h5>
+					</div>
+					<div className="signup__rightForm">
+						<form action="">
+							<TextField
+								className={classes.inputField}
+								variant="outlined"
+								placeholder="Email"
+							/>
+							<TextField
+								className={classes.inputField}
+								variant="outlined"
+								placeholder="Password"
+							/>
+							<div className="formBottom">
+								<Button className={classes.button} variant="contained">
+									Get Started
+								</Button>
+								<p>Have an Account?</p>
+							</div>
+						</form>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
