b2feb70a56ebdf14d2633f4ba977757cfba5d4ae
Andres Medrano <amedra13@gmail.com>
2021-03-07 19:09:14 -0800

    singleposts control own state and alsp update db

--- src/components/Feed/SinglePost.js
+++ src/components/Feed/SinglePost.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
 import RepeatOutlinedIcon from '@material-ui/icons/RepeatOutlined';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import PublishOutlinedIcon from '@material-ui/icons/PublishOutlined';
 
-const SinglePost = ({ post }) => {
+const SinglePost = ({ post, timePosted, increaseLike }) => {
+	const [comments, setComments] = useState(post.comments);
+	const [retweets, setRetweets] = useState(post.retweets);
+	const [favorite, setFavorite] = useState(post.favorite);
+	const [forwarded, setForwarded] = useState(post.forwarded);
 	return (
 		<div className="singlePost">
 			<div className="singlePost__avatarContainer">
@@ -14,8 +18,9 @@ const SinglePost = ({ post }) => {
 			<div className="singlePost__content">
 				<div className="singlePost__userInfo">
 					<h4>{post.user.name}</h4>
-					<h4 className="textColor">{post.user.username}</h4> *
-					<h4 className="textColor">{post.timePosted}</h4>
+					<h4 className="textColor">{post.user.username}</h4>{' '}
+					<span>&#183;</span>
+					<h4 className="textColor">{timePosted + 'm'}</h4>
 				</div>
 				<div className="singlePost__message">
 					<h3>{post.message}</h3>
@@ -25,29 +30,45 @@ const SinglePost = ({ post }) => {
 						<ModeCommentOutlinedIcon
 							fontSize="small"
 							style={{ margin: ' 0 10px' }}
+							onClick={() => {
+								setComments((prevState) => prevState + 1);
+								increaseLike(post._id, 'comments');
+							}}
 						/>{' '}
-						{post.comments}
+						{comments}
 					</div>
 					<div>
 						<RepeatOutlinedIcon
 							fontSize="small"
 							style={{ margin: ' 0 10px' }}
+							onClick={() => {
+								setRetweets((prevState) => prevState + 1);
+								increaseLike(post._id, 'retweets');
+							}}
 						/>{' '}
-						{post.retweets}
+						{retweets}
 					</div>
 					<div>
 						<FavoriteBorderOutlinedIcon
 							fontSize="small"
 							style={{ margin: ' 0 10px' }}
+							onClick={() => {
+								setFavorite((prevState) => prevState + 1);
+								increaseLike(post._id, 'favorite');
+							}}
 						/>{' '}
-						{post.favorite}
+						{favorite}
 					</div>
 					<div>
 						<PublishOutlinedIcon
 							fontSize="small"
 							style={{ margin: ' 0 10px' }}
+							onClick={() => {
+								setForwarded((prevState) => prevState + 1);
+								increaseLike(post._id, 'forwarded');
+							}}
 						/>{' '}
-						{post.forwarded}
+						{forwarded}
 					</div>
 				</div>
 			</div>
--- src/components/mainPage/Feed.js
+++ src/components/mainPage/Feed.js
@@ -1,14 +1,41 @@
 import React from 'react';
+// import axios from 'axios'
 import FeedHeader from '../Feed/FeedHeader';
 import SinglePost from '../Feed/SinglePost';
 import LoadingPost from '../Feed/LoadingPosts';
+import axios from 'axios';
 const Feed = ({ posts }) => {
+	const getTime = (date) => {
+		const datePosted = new Date(date);
+		const dateNow = new Date();
+		const diffInMilliSeconds = Math.abs(dateNow - datePosted) / 1000;
+		return Math.floor(diffInMilliSeconds / 60) % 60;
+	};
+
+	const incrementIcon = (id, icon) => {
+		axios
+			.post(`http://localhost:8080/post/${id}`, {
+				icon: icon,
+			})
+			.then((res) => {
+				console.log(res.data.message);
+			})
+			.catch((err) => console.log(err));
+	};
+
 	return (
 		<div className="feed">
 			<FeedHeader />
 			{posts ? (
 				posts.map((singlePost) => {
-					return <SinglePost key={singlePost._id} post={singlePost} />;
+					return (
+						<SinglePost
+							key={singlePost._id}
+							post={singlePost}
+							timePosted={getTime(singlePost.createdAt)}
+							increaseLike={incrementIcon}
+						/>
+					);
 				})
 			) : (
 				<LoadingPost />
