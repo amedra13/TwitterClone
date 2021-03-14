a7872755a83a05b08c631c4b509ec2b80097e73b
Andres Medrano <amedra13@gmail.com>
2021-03-13 18:06:05 -0800

    quick creatiion of login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	button: {
+		backgroundColor: '#00b4d8',
+		color: 'white',
+	},
+	icon: {
+		color: '#00b4d8',
+	},
+}));
+
+const CssTextField = withStyles({
+	root: {
+		'& .MuiOutlinedInput-root': {
+			margin: '10px 0',
+			'& fieldset': {
+				borderColor: '#00b4d8',
+			},
+			'&.Mui-focused fieldset': {
+				borderColor: '#00b4d8',
+			},
+		},
+	},
+})(TextField);
 
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const classes = useStyles();
+	const history = useHistory();
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		axios
+			.post('http://localhost:8080/login', {
+				email: email,
+				password: password,
+			})
+			.then((res) => {
+				console.log(res);
+				history.push(`/home/${res.data.user[0]._id}`);
+			})
+			.catch((err) => console.log(err));
+	};
 	return (
-		<div>
-			<h1>LogIn Conmponenet</h1>
+		<div className="login">
+			<div className="login__container">
+				<div className="login__header">
+					<TwitterIcon className={classes.icon} />
+					<h2>Login to Twitter</h2>
+				</div>
+				<form onSubmit={submitHandler}>
+					<CssTextField
+						variant="outlined"
+						placeholder="Email"
+						value={email}
+						fullWidth
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<CssTextField
+						variant="outlined"
+						placeholder="Password"
+						value={password}
+						fullWidth
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<Button type="submit" variant="outlined" className={classes.button}>
+						Login
+					</Button>
+				</form>
+			</div>
 		</div>
 	);
 };
