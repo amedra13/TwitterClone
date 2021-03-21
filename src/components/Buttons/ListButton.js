dab55a0ef5dd3741529a15f848f3bea622304eff
Andres Medrano <amedra13@gmail.com>
2021-03-20 22:27:17 -0700

    styled tweetStatus component

--- src/components/Buttons/ListButton.js
+++ src/components/Buttons/ListButton.js
@@ -1,30 +1,7 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
-	button: {
-		fontFamily: "'Raleway', sans-serif",
-		color: 'rgba(26, 26, 26, 0.795)',
-		backgroundColor: 'white',
-		'&:hover': {
-			color: '#00b4d8',
-			backgroundColor: 'rgba(243, 241, 241, 0.836)',
-			boxShadow: 'none',
-		},
-		margin: '10px 0',
-		boxShadow: 'none',
-		width: '75%',
-		borderRadius: '28px',
-		transition: 'all 250ms ease-in-out',
-		justifyContent: 'flex-start',
-	},
-}));
-
 const ListButton = ({ IconComponent, listItem, username, userId }) => {
-	const classes = useStyles();
-
 	const id = userId ? userId : username?.slice(1);
 
 	return (
--- src/pages/TweetStatus.js
+++ src/pages/TweetStatus.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Sidebar from '../components/mainPage/Sidebar';
 import Trends from '../components/mainPage/Trend';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import Avatar from '@material-ui/core/Avatar';
+import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
+import RepeatOutlinedIcon from '@material-ui/icons/RepeatOutlined';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import PublishOutlinedIcon from '@material-ui/icons/PublishOutlined';
 
 const TweetStatus = ({ user }) => {
 	const { postId } = useParams();
+	const [tweet, setTweet] = useState(null);
+
+	useEffect(() => {
+		const getTweet = async () => {
+			const response = await axios.get(
+				`http://localhost:8080/status/${postId}`
+			);
+			setTweet(response.data.tweet);
+		};
+		getTweet();
+	}, [postId]);
 	return (
 		<div className="tweetStatus">
 			<Sidebar username={user?.userName} userId={user?._id} />
 			<div className="tweet">
-				<h1>Tweet Component</h1>
-				<h4>PostID = {postId}</h4>
+				<div className="tweet__header">
+					<div className="tweet__headerInfo">
+						<Avatar>JT</Avatar>
+						<div>
+							<p>{tweet?.user.name}</p>
+							<p>{tweet?.user.username}</p>
+						</div>
+					</div>
+					<div className="message">
+						<p>{tweet?.message}</p>
+					</div>
+					<p className="time">{tweet?.createdAt}</p>
+					<div className="tweet__headerActivity">
+						<p>{tweet?.retweets} Retweets</p>
+						<p>{tweet?.favorite} Likes</p>
+					</div>
+					<div className="tweet__headerIcons">
+						<ModeCommentOutlinedIcon
+							style={{ color: 'rgba(136, 145, 150, 0.658)' }}
+						/>
+						<RepeatOutlinedIcon
+							style={{ color: 'rgba(136, 145, 150, 0.658)' }}
+						/>
+						<FavoriteBorderOutlinedIcon
+							style={{ color: 'rgba(136, 145, 150, 0.658)' }}
+						/>
+						<PublishOutlinedIcon
+							style={{ color: 'rgba(136, 145, 150, 0.658)' }}
+						/>
+					</div>
+				</div>
+				<div className="tweet__comments">{tweet?.comments}</div>
 			</div>
 			<Trends />
 		</div>
